502c519cd84bf621bfb2988106fb2aa1738e2ada
grethoremas3333 <grethoremas@gmail.com>
2022-06-07 12:15:58 +0100

    MAJ en cours implementations de nouveaux test le 07/06/2022 a 12:15

---
+++ src/main/java/com/parkit/parkingsystem/service/ThirtyMinutesForFree.java
@@ -0,0 +1,50 @@
+package com.parkit.parkingsystem.service;
+
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.model.Ticket;
+
+public class ThirtyMinutesForFree {
+
+    public void calculateFare(Ticket ticket){
+        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
+            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+        }
+
+        double daysDifference = ticket.getOutTime().getDay() - ticket.getInTime().getDay(); //on recupere le nombre de jours
+        ///System.out.println("le nombre de jour de difference est de: "+ daysDifference);
+
+        double inHour = ticket.getInTime().getHours(); /// on recupere la vaheur des heures HH de l'heure d'entree
+        double outHour = ticket.getOutTime().getHours(); /// on recupere la vaheur des heures HH de l'heure de sortie
+
+        double inMinutes = ticket.getInTime().getMinutes(); /// on recupere la vaheur des minutes MM de l'heure d'entree
+        double outMinutes = ticket.getOutTime().getMinutes(); /// on recupere la vaheur des minutes MM de l'heure de sortie
+
+        //TODO: Some tests are failing here. Need to check if this logic is correct
+        ///System.out.println("HelloICI");
+        inHour = (inHour * 60.0) + inMinutes;
+        //**/inHour = inHour - 30.0; //***/ a decommenter pour que le test passe au Vert
+        ///System.out.println("heure arrive: "+inHour); ///
+        outHour = (outHour * 60.0) + outMinutes;
+        ///System.out.println("heure sortie: "+outHour); ///
+
+        double duration = 0.0;
+
+        if ((outHour - inHour == 0.0) && (daysDifference == 0.0)) {
+            System.out.println("le vehicule est stationne depuis 30 minutes seulement donc il n'a rien a payer");
+            duration = ((outHour - inHour) + (daysDifference * 24.0 * 60.0)); // retire / 60.0 pour eviter exception
+        } else {
+            duration = (((outHour - inHour) + (daysDifference * 24.0 * 60.0)) / 60.0);
+        }
+        switch (ticket.getParkingSpot().getParkingType()){
+            case CAR: {
+                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                break;
+            }
+            case BIKE: {
+                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                break;
+            }
+            default: throw new IllegalArgumentException("Unkown Parking Type");
+        }
+    }
+}
---
+++ src/test/java/com/parkit/parkingsystem/ThirtyMinutesForFreeTest.java
@@ -0,0 +1,106 @@
+package com.parkit.parkingsystem;
+
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.FareCalculatorService;
+import com.parkit.parkingsystem.service.ThirtyMinutesForFree;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ThirtyMinutesForFreeTest {
+    private static ThirtyMinutesForFree fareCalculatorService;
+    private Ticket ticket;
+
+    @BeforeAll
+    private static void setUp() {
+        fareCalculatorService = new ThirtyMinutesForFree();
+    }
+
+    @BeforeEach
+    private void setUpPerTest() {
+        ticket = new Ticket();
+    }
+
+    @Test
+    public void calculateThirtyFareCar(){
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals(ticket.getPrice(), Fare.CAR_RATE_PER_HOUR);  //0.75
+        System.out.println("la valeur de ticket.getPrice est: "+ticket.getPrice()+" et celle de Fare.CarRate: "+Fare.CAR_RATE_PER_HOUR+" le test doit done 0.75 et le resultat attendu pour se scenario est 0.75 mais le Fare.CarRate doit donner 1.5 pour le moment");
+    }
+
+    @Test
+    public void calculateThirtyFareBike(){
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals(ticket.getPrice(), Fare.BIKE_RATE_PER_HOUR); //0.5
+    }
+
+    @Disabled
+    public void calculateFareBikeWithLessThanOneHourParkingTime(){
+        //Arrange
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  45 * 60 * 1000) );//45 minutes parking time should give 3/4th parking fare
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ///System.out.println("l'heure d'entree est: "+inTime); //
+        ///System.out.println(ticket.getInTime());
+        ///System.out.println("l'heure de sortie est: "+outTime); //
+        ///System.out.println(ticket.getOutTime());
+        ticket.setParkingSpot(parkingSpot);
+        //Act
+        fareCalculatorService.calculateFare(ticket);
+        //Assert
+        assertEquals((0.75 * Fare.BIKE_RATE_PER_HOUR), ticket.getPrice() );
+    }
+
+    @Disabled
+    public void calculateFareCarWithMoreThanADayParkingTime(){
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  24 * 60 * 60 * 1000) );//24 hours parking time should give 24 * parking fare per hour
+        ///System.out.println("la premiere valeur est de: "+inTime);
+        Date outTime = new Date();
+        ///System.out.println("la seconde valeur est de: "+outTime);
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals( (24 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
+    }
+
+}
+
+/*
+* la logique c'est d'utiliser la methode processExitingVehicle() mais au lieu de determiner la facture
+* avec la difference de temps obtenu on retranche encore 30 minute pour effectuer le calcul maintenant
+* deux cas de figure se pose si le temps (endTime[endtimeoriginal-30minutes] -startTime) > 0
+* on applique le calcul si < 0 le montant de la facture est 0 cela signifie que le client est reste
+* 30 minutes ou moins de 30 minutes stationne.
+* */
